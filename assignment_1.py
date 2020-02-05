df65e03b4b31e1e805db683a457608ec96efecb3
James Murphy <jmurphy@spartaglobal.com>
2020-02-05 16:55:47 +0000

    final assignment 1 @5

--- assignment_1.py
+++ assignment_1.py
@@ -2,6 +2,7 @@
 first_name = input('Your first name is...    ')
 last_name = input('Your surname is...    ')
 age = input('Age...    ')
+house_type = ('What sort of house do you live in?    ')
 mother_age = input("How old's your mum?    ")
 skill_1 = input("Now three skills, firstly...    ")
 skill_2 = input('Secondly...    ')
@@ -9,5 +10,6 @@ skill_3 = input('And finally...    ')
 full_name = first_name.capitalize() + " " + last_name.capitalize()
 skill_set = [skill_1, skill_2, skill_3]
 
-print(full_name + "'s bungalow with well kept lawn")
-
+print('So let me tell you about yourself...')
+print(f"Property information: {full_name}'s {house_type} with well kept lawn")
+print 
