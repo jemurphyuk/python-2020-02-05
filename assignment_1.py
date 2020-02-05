57ccbc6f605da2644fbfea0b6889015ae4aa7f49
James Murphy <jmurphy@spartaglobal.com>
2020-02-05 16:51:05 +0000

    initial data assignment 1

---
+++ assignment_1.py
@@ -0,0 +1,13 @@
+
+first_name = input('Your first name is...    ')
+last_name = input('Your surname is...    ')
+age = input('Age...    ')
+mother_age = input("How old's your mum?    ")
+skill_1 = input("Now three skills, firstly...    ")
+skill_2 = input('Secondly...    ')
+skill_3 = input('And finally...    ')
+full_name = first_name.capitalize() + " " + last_name.capitalize()
+skill_set = [skill_1, skill_2, skill_3]
+
+print(full_name + "'s bungalow with well kept lawn")
+
