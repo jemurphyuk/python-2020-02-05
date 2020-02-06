4904ad3e8ef1adef3041707f3e1ae8114ccc87fb
James Murphy <jmurphy@spartaglobal.com>
2020-02-06 09:46:45 +0000

    adding strip to gender input

--- assignment_2_story.py
+++ assignment_2_story.py
@@ -7,23 +7,23 @@ third_movie = input('What is your favourite Avengers movie, starring them all?  
 hero_gender = input('Are they male, female or prefer not to say?    ')
 hero_age = input('How old is your hero?    ')
 hero_story = {'beginning': first_movie,
-                   'middle': second_movie,
-                   'end': third_movie,
-                   'hero':
+                    'middle': second_movie,
+                    'end': third_movie,
+                    'hero':
                        {'name': marvel_hero,
                         'age': hero_age,
                         'gender': hero_gender,
+                        }
                     }
-                   }
 dictionary_hero = hero_story['hero']['name'].capitalize()
 print(dictionary_hero, hero_story['beginning'])
 if int(hero_age) > 40:
     print(dictionary_hero, hero_story['middle'], ': The Elder Story')
 else:
     print(dictionary_hero, hero_story['middle'], ': An Origin Story')
-if hero_gender.lower() == 'male':
+if hero_gender.lower().strip() == 'male':
     print(dictionary_hero, hero_story['end'], ': Man on a Mission')
-elif hero_gender.lower() == 'female':
+elif hero_gender.lower().strip() == 'female':
     print(dictionary_hero, hero_story['end'], ': Woman vs. The World')
 else:
     print(dictionary_hero, hero_story['end'], ': Gender Neutral Being vs. Society')
